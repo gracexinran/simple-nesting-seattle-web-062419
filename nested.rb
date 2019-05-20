b42bd0ca45fed95612035f4cb188c1be5c286735
Xinran Che <gracexinran@gmail.com>
2019-05-20 22:37:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+   programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+     programmer_hash
     
 end
 
@@ -103,7 +106,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay].keys[0] = :alans_new_info
+     programmer_hash[:alan_kay][:alans_new_info] = "GUI"
+     programmer_hash
      
 end
 
